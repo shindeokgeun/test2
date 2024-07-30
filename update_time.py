b9435be27cbab871fe29eb894890516f92cbbdda
shindeokgeun <shindeokgeun@gmail.com>
2024-07-30 13:30:07 +0900

    update_time.py 추가

---
+++ update_time.py
@@ -0,0 +1,7 @@
+﻿import datetime
+
+now = datetime.datetime.now()
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+with open("index.html", "w") as f:
+    f.write(f"Last updated at {current_time}")
